7e5a8897c7e5ea235c48144168f19687ea36fe9a
Hong Ng <hong.yang.ng@gmail.com>
2023-04-26 21:43:08 +1000

    Add function to repeat strings and enable all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, numOfRepeats) {
+    let result = "";
 
+    if(numOfRepeats > 0){
+        for(let i=0; i<numOfRepeats; i++){
+            result += stringToRepeat;
+        }
+
+        return result;
+    }
+
+    else if(numOfRepeats == 0){
+        return "";
+    }
+
+    else{
+        return "ERROR";
+    }
 };
+
+// console.log(repeatString("you", 10));
 
 // Do not edit below this line
 module.exports = repeatString;
