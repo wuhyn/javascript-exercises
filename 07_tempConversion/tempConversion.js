0ecad68964f738dabcff6f48c9ba70b1880f42e4
Hong Ng <hong.yang.ng@gmail.com>
2023-05-07 17:51:05 +1000

    Add logic to convert temperature from fahreinheit to celsius and vice versa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = 0;
+
+  celsius = (temp - 32) * 5/9;
+
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = 0;
+
+  fahrenheit = (temp * 9/5) + 32;
+
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+
+  return fahrenheit;
 };
 
 // Do not edit below this line
