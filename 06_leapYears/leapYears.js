f15078f55a3ad4fe48129add6046f3389c7e59fa
Hong Ng <hong.yang.ng@gmail.com>
2023-05-07 17:35:10 +1000

    Implement logic to check if a given year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(year % 4 == 0 && year % 100 != 0){
+        return true;
+    }
 
+    else if(year % 400 == 0){
+        return true;
+    }
+
+    else {
+        return false;
+    }
 };
+
+leapYears(1900);
 
 // Do not edit below this line
 module.exports = leapYears;
