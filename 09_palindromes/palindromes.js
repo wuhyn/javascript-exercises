76398c59e8ac2a7589ae98ffc8a9f719b3179a3f
Hong Ng <hong.yang.ng@gmail.com>
2023-05-28 10:38:01 +1000

    Add palindrome function logic

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,28 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    let strLength = inputString.length - 1;
+    // let newString = inputString.replace(" ", "");
+    let reverseString = "";
+    let newString = ""
+    
+    for(let i=strLength; i>=0; i--){
+        if(inputString.charAt(i) !== " ")
+            reverseString += inputString[i];
+    }
+
+    for (let i=0; i<= strLength; i++){
+        if(inputString.charAt(i) !== " ")
+            newString += inputString[i];
+    }
+
+    //Strip special characters and ensure string is in lower case
+    newString = newString.replace(/[^\w ]/g, '').toLowerCase();
+    reverseString = reverseString.replace(/[^\w ]/g, '').toLowerCase();
+
+    // Perform palindrome check
+    if(newString === reverseString)
+        return true;
+    else
+        return false;
 
 };
 
