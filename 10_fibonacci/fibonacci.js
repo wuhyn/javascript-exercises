099b5b23be017fd2db3de1beee16a6d33bea3a60
Hong Ng <hong.yang.ng@gmail.com>
2023-05-28 13:28:49 +1000

    Add fibonacci logic

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,38 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fibNum = parseInt(num);
+    let n1 = 1;
+    let n2 = 1;
+    let next = 0;
+    let result;
+    
+    if(fibNum < 0){
+        console.log("oops.");
+        return "OOPS";
+    }
 
+    for(let i=0; i<=fibNum; i++){
+        if(i == 0){
+            result = 0;
+        }
+
+        else if(i == 1 || i == 2){
+            result = 1;
+        }
+
+        else{
+            next = n1 + n2;
+            result = next;
+
+            //Move to next sequence
+            n1 = n2; // 1
+            n2 = next; // 2
+           
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+fibonacci("1");
