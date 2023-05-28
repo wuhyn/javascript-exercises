db29a1c6fe89bccafd07da65ef6e7678ee57c64f
Hong Ng <hong.yang.ng@gmail.com>
2023-05-28 13:48:34 +1000

    Add logic to loop array and push the title property to the new array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const titles = [];
 
+    books.forEach(book => {
+        titles.push(book.title);  
+    });
+
+    return titles;
 };
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
